919d8fd045542f9da803c73b147d1c3934a6f1d9
glebys33 <glebys.kiselev@gmail.com>
2022-04-06 23:09:25 +0300

    3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ursina import *
 from Pyramid_4 import Piramid_4
-from math import acos, degrees
+from math import acos, atan, degrees
 
 
 class Area(Ursina):
@@ -15,17 +15,23 @@ class Area(Ursina):
         self.d = None
         self.P = None
         self.K = None
+        self.N = None
         self.PK = None
         self.AK = None
         self.BP = None
+        self.SN = None
         self.txP = None
         self.tP = None
         self.txK = None
         self.tK = None
+        self.txN = None
+        self.tN = None
+        self.txT = None
+        self.tT = None
         self.load_area()
 
     def load_area(self):
-        self.piramid = Piramid_4(40, 20)
+        self.piramid = Piramid_4(40, 30)
         self.piramid.build()
         self.a = self.piramid.a
         self.h = self.piramid.h
@@ -43,23 +49,46 @@ class Area(Ursina):
                         z=self.a / 4)
         self.K = Entity(model='sphere', color=color.black, scale=0.2, x=self.a / 4, y=self.h / 2,
                         z=-self.a / 4)
+        self.N = Entity(model='sphere', color=color.black, scale=0.2, x=self.a / 2)
+        del_z = degrees(atan(self.h / (self.a/2)))
+        self.SN = Entity(model=Cylinder(), color=color.black, scale=(0.1, (self.d**2 - self.a**2/4)**0.5, 0.1),
+                         rotation_z=del_z-90, x=self.a/2)
+        self.piramid.rs.append(self.SN)
         self.PK = Entity(model=Cylinder(), color=color.black, scale=(0.1, self.a / 2, 0.1), rotation_x=90,
                          x=self.K.x, y=self.K.y, z=self.K.z)
+        self.piramid.rs.append(self.PK)
         AK = (self.d**2 * (0.25 + self.a ** 2 / (2 * self.d ** 2))) ** 0.5
         del_x = degrees(acos(self.h / (2*AK)))
         self.AK = Entity(model=Cylinder(), color=color.black, scale=(0.1, AK, 0.1), rotation_y=71.5, rotation_x=del_x,
                          x=-self.a/2, z=-self.a/2)
+        self.piramid.rs.append(self.AK)
         self.BP = Entity(model=Cylinder(), color=color.black, scale=(0.1, AK, 0.1), rotation_y=108.5, rotation_x=del_x,
                          x=-self.a/2, z=self.a/2)
-        self.txP = Text(text="P", x=self.a / 4, z=self.a / 4, y=self.h / 2, background=False, parent=scene, scale=50,
+        self.piramid.rs.append(self.BP)
+        self.txP = Text(text="P", x=self.a / 4, z=self.a / 3.73, y=self.h / 2, background=False, parent=scene, scale=50,
                         color=color.red)
+        self.piramid.ts.append(self.txP)
         self.tP = Text(text="P", x=self.a / 4, z=self.a / 4, y=self.h / 2, background=False, parent=scene, scale=50,
                        color=color.red, rotation_y=180)
-        self.txK = Text(text="K", x=self.a / 4, y=self.h / 2, z=-self.a / 3.73, background=False, parent=scene,
-                        scale=50,
+        self.piramid.ts.append(self.tP)
+        self.txK = Text(text="K", x=self.a / 4, y=self.h / 2, z=-self.a / 3.73, background=False, parent=scene, scale=50,
                         color=color.red)
+        self.piramid.ts.append(self.txK)
         self.tK = Text(text="K", x=self.a / 4, y=self.h / 2, z=-self.a / 4, background=False, parent=scene, scale=50,
                        color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.tK)
+        self.txN = Text(text="N", x=self.a / 2, y=-0.05, background=False, parent=scene, scale=50,
+                        color=color.red)
+        self.piramid.ts.append(self.txN)
+        self.tN = Text(text="N", x=self.a / 2, y=-0.05, background=False, parent=scene, scale=50,
+                       color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.tN)
+        self.txT = Text(text="T", x=self.a / 4 + 0.1, y=self.h/2 + 1, background=False, parent=scene, scale=50,
+                        color=color.red)
+        self.piramid.ts.append(self.txT)
+        self.tT = Text(text="T", x=self.a / 4 + 0.1, y=self.h/2 + 1, background=False, parent=scene, scale=50,
+                       color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.tT)
 
     def input(self, key):
         super().input(key)
