777f5d5505d0a4cb2d80f8e5e34d66bad6d47507
glebys33 <glebys.kiselev@gmail.com>
2022-04-06 22:51:21 +0300

    2

--- Pyramid_4.py
+++ Pyramid_4.py
@@ -3,36 +3,36 @@ from math import atan, degrees
 
 
 class Piramid_4(Ursina):
-    def __init__(self, a=20, h=15, r_color=color.gray, d_color=color.gray, h_color=color.gray):
+    def __init__(self, a=20, h=15, r_color=color.black, d_color=color.black, h_color=color.black):
         self.a = a # длина стороны квадрата
         self.h = h # длинна высоты
         self.r_color = r_color # цвет рёбер
         self.d_color = d_color # цвет диогоналей
         self.h_color = h_color # цвет высоты
         self.rs = [] # диогонали
+        self.ts = []
         self.d1 = None # 1-я диогональ
         self.d2 = None # 2-я диогональ
         self.hp = None # высота
-        self.d = None
 
     def build(self):
         # основание
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, x=-10, z=-10)
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, x=-self.a/2, z=-self.a/2)
         self.rs.append(r)
         r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, rotation_y=90,
-                   x=-10, z=-10)
+                   x=-self.a/2, z=-self.a/2)
         self.rs.append(r)
         r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, rotation_y=90,
-                   x=-10, z=10)
+                   x=-self.a/2, z=self.a/2)
         self.rs.append(r)
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, x=10, z=-10)
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.a, 0.1), rotation_x=90, x=self.a/2, z=-self.a/2)
         self.rs.append(r)
 
         # диогонали
         self.d1 = Entity(model=Cylinder(), color=self.d_color, scale=(0.1, self.a*2**0.5, 0.1), rotation_x=90,
-                         rotation_y=45 ,x=-10, z=-10)
+                         rotation_y=45 ,x=-self.a/2, z=-self.a/2)
         self.d2 = Entity(model=Cylinder(), color=self.d_color, scale=(0.1, self.a*2**0.5, 0.1), rotation_x=90,
-                         rotation_y=-45 ,x=10, z=-10)
+                         rotation_y=-45 ,x=self.a/2, z=-self.a/2)
 
         # высота
         self.hp = Entity(model=Cylinder(), color=self.h_color, scale=(0.1, self.h, 0.1))
@@ -40,21 +40,49 @@ class Piramid_4(Ursina):
         # ребра
         self.d = (self.h**2 + self.a**2/2)**0.5 # длина ребра
         del_x = 90 - degrees(atan(2*self.h / (self.a*2**0.5)))
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=-10, z=-10, rotation_x=del_x,
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=-self.a/2, z=-self.a/2, rotation_x=del_x,
                    rotation_y=45)
         self.rs.append(r)
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=-10, z=10, rotation_x=-del_x,
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=-self.a/2, z=self.a/2, rotation_x=-del_x,
                    rotation_y=-45)
         self.rs.append(r)
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=10, z=-10, rotation_x=del_x,
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=self.a/2, z=-self.a/2, rotation_x=del_x,
                    rotation_y=-45)
         self.rs.append(r)
-        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=10, z=10, rotation_x=-del_x,
+        r = Entity(model=Cylinder(), color=self.r_color, scale=(0.1, self.d, 0.1), x=self.a/2, z=self.a/2, rotation_x=-del_x,
                    rotation_y=45)
         self.rs.append(r)
 
+        # вершины
+        r = Text(text="C",x=self.a/2 + 0.1,z=self.a/2 + 0.05, background=False,parent=scene,scale=50,color=color.red)
+        self.ts.append(r)
+        r = Text(text="A", x=-self.a/2 - 0.1, z=-self.a/2 - 0.05, background=False, parent=scene, scale=50, color=color.red)
+        self.ts.append(r)
+        r = Text(text="D", x=self.a/2 + 0.1, z=-self.a/2 - 0.05, background=False, parent=scene, scale=50, color=color.red)
+        self.ts.append(r)
+        r = Text(text="B", x=-self.a/2 - 0.1, z=self.a/2 - 0.05, background=False, parent=scene, scale=50, color=color.red)
+        self.ts.append(r)
+        r = Text(text="S", y=self.h + 1.05, background=False, parent=scene, scale=50, color=color.red)
+        self.ts.append(r)
+        r = Text(text="C", x=self.a/2 + 0.1, z=self.a/2 + 0.05, background=False, parent=scene, scale=50, color=color.red,rotation_y=180)
+        self.ts.append(r)
+        r = Text(text="A", x=-self.a/2 - 0.1, z=-self.a/2 - 0.05, background=False, parent=scene, scale=50, color=color.red,rotation_y=180)
+        self.ts.append(r)
+        r = Text(text="D", x=self.a/2 + 0.1, z=-self.a/2 - 0.05, background=False, parent=scene, scale=50, color=color.red,rotation_y=180)
+        self.ts.append(r)
+        r = Text(text="B", x=-self.a/2 - 0.1, z=self.a/2 + 0.05, background=False, parent=scene, scale=50, color=color.red,rotation_y=180)
+        self.ts.append(r)
+        r = Text(text="S", y=self.h + 1.05, background=False, parent=scene, scale=50, color=color.red,rotation_y=180)
+        self.ts.append(r)
+        r = Text(text="H", x=-0.4,background=False, parent=scene, scale=50, color=color.red)
+        self.ts.append(r)
+        r = Text(text="H", x=0.4,background=False, parent=scene, scale=50, color=color.red, rotation_y=180)
+        self.ts.append(r)
+
     def off_all(self):
         for i in self.rs:
+            i.disable()
+        for i in self.ts:
             i.disable()
         self.hp.disable()
         self.d1.disable()
@@ -80,6 +108,8 @@ class Piramid_4(Ursina):
     def on_all(self):
         for i in self.rs:
             i.enable()
+        for i in self.ts:
+            i.enable()
         self.hp.enable()
         self.d1.enable()
         self.d2.enable()
@@ -100,3 +130,4 @@ class Piramid_4(Ursina):
     def on_rs(self):
         for i in self.rs:
             i.enable()
+            
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import colorama
 from ursina import *
 from Pyramid_4 import Piramid_4
 from math import acos, degrees
@@ -8,20 +7,38 @@ class Area(Ursina):
     def __init__(self):
         super().__init__()
         window.color = color.white
-        Entity(model='quad', color=color.light_gray, scale=200, y=-5, rotation_x=90, texture='white_cube',
-               texture_scale=(200, 200))
         EditorCamera()
-        camera.world_position = (0, 0, 15)
+        camera.world_position = (0, 3, 15)
+        self.piramid = None
+        self.a = None
+        self.h = None
+        self.d = None
+        self.P = None
+        self.K = None
+        self.PK = None
+        self.AK = None
+        self.BP = None
+        self.txP = None
+        self.tP = None
+        self.txK = None
+        self.tK = None
         self.load_area()
 
     def load_area(self):
-        self.piramid = Piramid_4()
+        self.piramid = Piramid_4(40, 20)
         self.piramid.build()
         self.a = self.piramid.a
         self.h = self.piramid.h
         self.d = self.piramid.d
 
     def etap1(self):
+        for i in self.piramid.rs:
+            i.color = color.gray
+        self.piramid.hp.color = color.gray
+        self.piramid.d1.color = color.gray
+        self.piramid.d2.color = color.gray
+        for i in self.piramid.ts:
+            i.color = color.black
         self.P = Entity(model='sphere', color=color.black, scale=0.2, x=self.a / 4, y=self.h / 2,
                         z=self.a / 4)
         self.K = Entity(model='sphere', color=color.black, scale=0.2, x=self.a / 4, y=self.h / 2,
@@ -30,19 +47,24 @@ class Area(Ursina):
                          x=self.K.x, y=self.K.y, z=self.K.z)
         AK = (self.d**2 * (0.25 + self.a ** 2 / (2 * self.d ** 2))) ** 0.5
         del_x = degrees(acos(self.h / (2*AK)))
-        self.AK = Entity(model=Cylinder(), color=color.black, scale=(
-        0.1, AK, 0.1), rotation_y=71.5, rotation_x=del_x, x=-10, z=-10)
+        self.AK = Entity(model=Cylinder(), color=color.black, scale=(0.1, AK, 0.1), rotation_y=71.5, rotation_x=del_x,
+                         x=-self.a/2, z=-self.a/2)
+        self.BP = Entity(model=Cylinder(), color=color.black, scale=(0.1, AK, 0.1), rotation_y=108.5, rotation_x=del_x,
+                         x=-self.a/2, z=self.a/2)
+        self.txP = Text(text="P", x=self.a / 4, z=self.a / 4, y=self.h / 2, background=False, parent=scene, scale=50,
+                        color=color.red)
+        self.tP = Text(text="P", x=self.a / 4, z=self.a / 4, y=self.h / 2, background=False, parent=scene, scale=50,
+                       color=color.red, rotation_y=180)
+        self.txK = Text(text="K", x=self.a / 4, y=self.h / 2, z=-self.a / 3.73, background=False, parent=scene,
+                        scale=50,
+                        color=color.red)
+        self.tK = Text(text="K", x=self.a / 4, y=self.h / 2, z=-self.a / 4, background=False, parent=scene, scale=50,
+                       color=color.red, rotation_y=180)
 
     def input(self, key):
         super().input(key)
         if key == '1':
             self.etap1()
-        elif key == '=':
-            self.AK.rotation_x -= 1
-        elif key == 'q':
-            self.AK.rotation_y += 1
-        elif key == 'a':
-            self.AK.rotation_y -= 1
 
 
 if __name__ == '__main__':
