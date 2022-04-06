f01b60c22e91b938e79dda6c464c306b989cad38
glebys33 <glebys.kiselev@gmail.com>
2022-04-07 00:41:16 +0300

    4

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from ursina import *
 from Pyramid_4 import Piramid_4
-from math import acos, atan, degrees
+from math import acos, atan, sin, cos, degrees, radians
 
 
 class Area(Ursina):
     def __init__(self):
         super().__init__()
+        window.fullscreen = True
         window.color = color.white
         EditorCamera()
-        camera.world_position = (0, 3, 15)
+        camera.world_position = (0, 15, 15)
         self.piramid = None
         self.a = None
         self.h = None
@@ -28,6 +29,9 @@ class Area(Ursina):
         self.tN = None
         self.txT = None
         self.tT = None
+        self.pl = None
+        self.plx = None
+        self.perp = None
         self.load_area()
 
     def load_area(self):
@@ -90,10 +94,97 @@ class Area(Ursina):
                        color=color.red, rotation_y=180)
         self.piramid.ts.append(self.tT)
 
+    def etap2(self):
+        self.etap1()
+        for i in self.piramid.rs:
+            i.color = color.gray
+        for i in self.piramid.ts:
+            i.color = color.black
+        del_x = degrees(atan(self.h / (self.a / 2)))
+        self.pl = Entity(model='quad', color=color.light_gray, scale=(self.a + 5, self.SN.scale[1] + 10),
+                         x=-self.a/4, y=self.h/2, rotation_y=90, rotation_x=90-del_x)
+        self.plx = Entity(model='quad', color=color.light_gray, scale=(self.a + 5, self.SN.scale[1] + 10),
+                          x=-self.a / 4, y=self.h / 2, rotation_y=-90, rotation_x=del_x-90)
+        self.perp = Entity(model=Cylinder(), color=color.black, scale=(0.1, self.a/2 * sin(radians(del_x)), 0.1),
+                           x=self.P.x, y=self.P.y, z=self.P.z,
+                         rotation_z=-del_x)
+
+    def etap3(self):
+        self.etap1()
+        for i in self.piramid.rs:
+            i.color = color.gray
+        for i in self.piramid.ts:
+            i.color = color.black
+        try:
+            self.AK.disable()
+            self.PK.disable()
+            self.BP.disable()
+        except AttributeError:
+            pass
+        self.M = Entity(model='sphere', color=color.black, scale=0.2, x=-self.a / 2)
+        self.txm = Text(text="M", x=self.M.x + 0.1, y=self.M.y + 0.1, z=self.M.z + 0.1, background=False, parent=scene,
+                        scale=50,
+                        color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.txm)
+        self.tm = Text(text="M", x=self.M.x - 1, y=self.M.y + 0.1, z=self.M.z + 0.1, background=False, parent=scene,
+                       scale=50, color=color.red)
+        self.piramid.ts.append(self.tm)
+        self.MN = Entity(model=Cylinder(), color=color.black, scale=(0.1, self.a, 0.1), rotation_z=90,
+                         x=-self.a / 2)
+        self.piramid.rs.append(self.MN)
+        del_z = degrees(atan(self.h / (self.a / 2)))
+        self.MS = Entity(model=Cylinder(), color=color.black, scale=(0.1, (self.d**2 - self.a**2/4)**0.5, 0.1),
+                         rotation_z=90-del_z, x=-self.a/2)
+        self.piramid.rs.append(self.MS)
+        self.NQ = Entity(model=Cylinder(), color=color.black, scale=(0.1, self.a * sin(radians(del_z)), 0.1),
+                         x=self.N.x, y=self.N.y, z=self.N.z, rotation_z=-del_z)
+        self.piramid.rs.append(self.NQ)
+        self.Q = Entity(model='sphere', color=color.black, scale=0.2,
+                        x=self.a / 2 - self.NQ.scale[1] * cos(radians(90 - del_z)),
+                        y=self.NQ.scale[1] * sin(radians(90 - del_z)))
+        self.txQ = Text(text="Q", x=self.Q.x - 0.8, y=self.Q.y + 1, background=False, parent=scene, scale=50,
+                        color=color.red)
+        self.piramid.ts.append(self.txQ)
+        self.tQ = Text(text="Q", x=self.Q.x - 0.5, y=self.Q.y + 1, background=False, parent=scene, scale=50,
+                       color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.tQ)
+        self.TJ = Entity(model=Cylinder(), color=color.black, scale=(0.1, self.a/2 * sin(radians(del_z)), 0.1),
+                         x=self.tT.x - 0.1, y=self.tT.y - 1, z=self.tT.z, rotation_z=-del_z)
+        self.piramid.rs.append(self.TJ)
+        self.J = Entity(model='sphere', color=color.black, scale=0.2,
+                        x=self.a / 4 - self.TJ.scale[1] * cos(radians(90 - del_z)),
+                        y=self.h/2 + self.TJ.scale[1] * sin(radians(90 - del_z)))
+        self.txj = Text(text="J", x=self.J.x - 0.8, y=self.J.y + 1, z=self.J.z + 0.1, background=False, parent=scene,
+                        scale=50, color=color.red, rotation_y=180)
+        self.piramid.ts.append(self.txj)
+        self.tj = Text(text="J", x=self.J.x - 0.5, y=self.J.y + 1, z=self.J.z + 0.1, background=False, parent=scene,
+                        scale=50, color=color.red)
+        self.piramid.ts.append(self.tj)
+
     def input(self, key):
         super().input(key)
         if key == '1':
             self.etap1()
+        elif key == '2':
+            self.etap2()
+        elif key == '3':
+            try:
+                self.perp.disable()
+            except AttributeError:
+                pass
+            self.etap3()
+        elif key == '4':
+            self.etap1()
+            self.etap3()
+            try:
+                self.pl.disable()
+                self.plx.disable()
+            except AttributeError:
+                pass
+            for i in self.piramid.rs:
+                i.color = color.gray
+            for i in self.piramid.ts:
+                i.color = color.black
 
 
 if __name__ == '__main__':
